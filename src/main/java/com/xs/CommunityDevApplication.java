3e5208d1d6d9d32148427972be97fb9cd8662976
uncleX1999 <382471119@qq.com>
2020-02-27 16:57:42 +0800

    init repo

---
+++ src/main/java/com/xs/CommunityDevApplication.java
@@ -0,0 +1,13 @@
+package com.xs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityDevApplication.class, args);
+    }
+
+}
