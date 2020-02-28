360938bc70be88b6ca6ebf8fb629f7c02949dc69
Shore001 <stevengod001@gmail.com>
2020-02-28 21:06:15 +0800

    update session

--- src/main/java/com/xs/controller/IndexController.java
+++ src/main/java/com/xs/controller/IndexController.java
@@ -1,16 +1,36 @@
 package com.xs.controller;
 
 
+import com.xs.mapper.UserMapper;
+import com.xs.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
     //访问根目录时返回结果
+
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public String hello(){
+    public String hello(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie:cookies){
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user!=null){//用户信息放入Session中
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
--- src/main/java/com/xs/mapper/UserMapper.java
+++ src/main/java/com/xs/mapper/UserMapper.java
@@ -4,9 +4,13 @@ package com.xs.mapper;
 import com.xs.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into USER(name,account_id,token,gmt_create,gmt_modify) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModify})")
     public void insert(User user);
+
+    @Select("select * from USER where TOKEN=#{token}")
+    User findByToken(String token);
 }
